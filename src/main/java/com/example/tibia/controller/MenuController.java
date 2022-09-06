e32cbe0370404ed99d9218d33ab38c5a0b4ab420
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-09-06 13:39:32 +0200

    refactor

---
+++ src/main/java/com/example/tibia/controller/MenuController.java
@@ -0,0 +1,38 @@
+package com.example.tibia.controller;
+
+import com.example.tibia.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class MenuController {
+
+    @FXML
+    private Button buttonStartGame;
+
+    @FXML
+    private Button buttonLoadGame;
+
+    @FXML
+    private Button buttonExit;
+
+    @FXML
+    void exit(ActionEvent event) {
+        Main.closeView();
+
+    }
+
+    @FXML
+    void loadGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void startGame(ActionEvent event) throws IOException {
+        Main.setNameSelectView();
+
+    }
+
+}
